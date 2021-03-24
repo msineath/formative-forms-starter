50056d2085956245c55ad60f189a90b716a829ae
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-03-24 19:33:22 -0400

    working on create-interesting

--- index.js
+++ index.js
@@ -48,23 +48,65 @@ app.post('/create', csurfProtection, (req, res) => {
   if(!password) {
     errors.push("Please provide a password.");
   }
-
   if (password!==confirmedPassword) {
     errors.push("The provided values for the password and password confirmation fields did not match.")
   }
-
-if(errors.length > 0) {
-  res.render('create', {errors, firstName, lastName, email});
-  return;  
+  if(errors.length > 0) {
+    res.render('create', {errors, firstName, lastName, email});
+    return;  
 }
-
-
-
   let newUser = {
     id: users.length + 1,
     firstName,
     lastName,
     email
+  };
+  users.push(newUser);
+  res.redirect('/');
+})
+
+app.get('/create-interesting', csurfProtection, (req, res) => {
+  res.render('create-interesting', {csrfToken: req.csrfToken(), errors});
+});
+
+app.post('/create-interesting', csurfProtection, (req, res) => {
+  errors = [];
+  const {firstName, lastName, email, age, favoriteBeatle, password, confirmedPassword} = req.body;
+  if(!firstName) {
+    errors.push("Please provide a first name.");
+  }
+  if(!lastName) {
+    errors.push("Please provide a last name.");
+  }
+  if(!email) {
+    errors.push("Please provide an email.");
+  }
+  if(!age) {
+    errors.push("Please provide an age.");
+  }
+  if(!favoriteBeatle) {
+    errors.push("Please provide a favorite Beatle.")
+  }
+  if(!password) {
+    errors.push("Please provide a password.");
+  }
+  if (password!==confirmedPassword) {
+    errors.push("The provided values for the password and password confirmation fields did not match.")
+  }
+  if(age < 0 || age > 120) {
+    errors.push("Please provide a valid age.")
+  }
+  if(errors.length > 0) {
+    res.render('create', {errors, firstName, lastName, email, age, favoriteBeatle});
+    return;  
+  }
+  let newUser = {
+    id: users.length + 1,
+    firstName,
+    lastName,
+    email,
+    age,
+    favoriteBeatle
   };
   users.push(newUser);
   res.redirect('/');
