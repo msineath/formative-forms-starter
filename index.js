23b8774de2e55052289fe15a8f7e30d615b9a641
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-03-24 17:42:56 -0400

    trying to figure out errors import

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
-const csurf = require('csurf');
 const cookieParser = require('cookie-parser');
+const csurf = require('csurf');
 const csurfProtection = csurf({cookie: true});
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+
 app.use(cookieParser());
 
 app.set("view engine", "pug");
 
+
 app.get("/", (req, res) => {
   res.render('index', {users});
 });
+
+
 
 const users = [
   {
@@ -21,6 +27,41 @@ const users = [
   }
 ];
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/create', csurfProtection, (req, res) => {
+  res.render('create', {csrfToken: req.csrfToken()});
+});
 
+app.post('/create', csurfProtection, (req, res) => {
+  const {firstName, lastName, email, password} = req.body;
+  let errors = [];
+  if(!firstName) {
+    errors.push("Please provide a first name.");
+  }
+  if(!lastName) {
+    errors.push("Please provide a last name.");
+  }
+  if(!email) {
+    errors.push("Please provide an email.");
+  }
+  if(!password) {
+    errors.push("Please provide a password.");
+  }
+
+if(errors.length > 0) {
+  res.render('create', {title: 'The following errors were found:', errors});
+  return;  
+}
+
+  let newUser = {
+    id: users.length + 1,
+    firstName,
+    lastName,
+    email
+  };
+  users.push(newUser);
+  res.redirect('/');
+})
+
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  
 module.exports = app;
--- routes.js
+++ routes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const csurf = require('csurf');
+const csurfProtection = csurf({cookie: true});
 
-router.get('/create', (req, res) => {
-    res.render();
+router.get('/create', csurfProtection, (req, res) => {
+    res.render('create', {csrfToken: req.csrfToken()});
 });
+
+module.exports = router;
